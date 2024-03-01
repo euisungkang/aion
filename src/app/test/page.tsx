2d33878288de1fe855354ec3a5906dde1e01c782
Easton Kang <euisungkang@gmail.com>
2024-03-01 23:37:00 +0900

    feat: landing page completed '/'

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import { BackgroundBeams} from "@/components/ui/background-beams";
+
+export default function Test() {
+    return (
+        <main className="dark">
+            <div className="h-screen w-screen bg-[#141414]">
+                <BackgroundBeams />
+            </div>
+        </main>
+    );
+}
